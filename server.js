9de018e508d0bbc8f89780a26c4e9de42b7c6905
Kishan Gohil <gohilkishan159@gmail.com>
2020-07-22 20:56:18 +0530

    production update

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const connectDb = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 //connect to Database
 connectDb();
 
-const PORT = 5000 || process.env.PORT;
-
 app.use(express.json({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("App is running");
-});
 
 //Defining routes
 app.use("/api/register", require("./routes/register"));
@@ -20,6 +15,16 @@ app.use("/api/auth", require("./routes/auth"));
 
 //post route
 app.use("/api/post", require("./routes/post"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("front_end/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "front_end", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
